51dbc1e34e2e374df3b772ac3410ef30f40350f9
phil <jaeja.phil@gmail.com>
2024-04-12 13:35:00 +0900

    fix(PerformanceSeat): apply added field and check validation methods
    - add isAvailable method which checks whether performance seat can be booked
    - add book which sets fields to be booked

--- src/main/kotlin/com/example/hhplusweek3/domain/performanceSeat/PerformanceSeat.kt
+++ src/main/kotlin/com/example/hhplusweek3/domain/performanceSeat/PerformanceSeat.kt
@@ -1,15 +1,20 @@
 package com.example.hhplusweek3.domain.performanceSeat
 
 import com.example.hhplusweek3.domain.concertPerformance.ConcertPerformance
+import com.example.hhplusweek3.domain.performanceSeatBookInfo.PerformanceSeatBookInfo
 import com.example.hhplusweek3.domain.user.User
+import com.example.hhplusweek3.domain.userQueueToken.UserQueueToken
 import com.example.hhplusweek3.entity.performanceSeat.PerformanceSeatEntity
+import com.example.hhplusweek3.error.BadRequestException
+import java.time.LocalDateTime
 
 data class PerformanceSeat(
     val id: Long,
     val concertPerformance: ConcertPerformance,
     val seatNumber: Int,
     val user: User?,
-    val booked: Boolean
+    val booked: Boolean,
+    val performanceSeatBookInfo: PerformanceSeatBookInfo?
 ) {
     fun toEntity(): PerformanceSeatEntity {
         return PerformanceSeatEntity(
@@ -17,7 +22,42 @@ data class PerformanceSeat(
             concertPerformance = concertPerformance.toEntity(),
             seatNumber = seatNumber,
             user = user?.toEntity(),
-            booked = booked
+            booked = booked,
+            performanceSeatBookInfo = performanceSeatBookInfo?.toEntity()
+        )
+    }
+
+    fun isAvailable(): Boolean {
+        val now = LocalDateTime.now()
+
+        if (booked ||
+            concertPerformance.performanceDateTime.isBefore(now) ||
+            performanceSeatBookInfo?.bookSuccessAt != null) {
+            return false
+        }
+
+        if (performanceSeatBookInfo?.bookAttemptAt != null &&
+            performanceSeatBookInfo.bookAttemptAt.isAfter(now.minusMinutes(5))) {
+            return false
+        }
+
+        return true
+    }
+
+    fun book(userQueueToken: UserQueueToken): PerformanceSeat {
+        if (!isAvailable()) {
+            throw BadRequestException("Performance seat is not available to book.")
+        }
+
+        val now = LocalDateTime.now()
+        return copy(
+            user = userQueueToken.user,
+            performanceSeatBookInfo = PerformanceSeatBookInfo(
+                performanceSeat = this,
+                token = userQueueToken.token,
+                bookAttemptAt = now,
+                bookSuccessAt = null
+            )
         )
     }
 }
