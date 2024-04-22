58cf16d79d1d37cc8dde115b09a9ecb3e768f8ad
phil <jaeja.phil@gmail.com>
2024-04-22 14:21:33 +0900

    feat(): Add Healthcheck endpoint

---
+++ src/main/kotlin/com/example/hhplusweek3/controller/HealthCheckController.kt
@@ -0,0 +1,14 @@
+package com.example.hhplusweek3.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthCheckController {
+
+    @RequestMapping("/check")
+    fun check(): String {
+        return "OK"
+    }
+}
